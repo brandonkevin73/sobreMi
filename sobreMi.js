f64ff407b60fb9d3bcb20f6697c87875d0bdd6ef
brandonkevin73 <132932443+brandonkevin73@users.noreply.github.com>
2023-07-15 05:29:03 -0300

    Add files via upload

---
+++ sobreMi.js
@@ -0,0 +1,18 @@
+const sobreMi = {
+    nombre: "brandon",
+    edad: 20,
+    "eres-desarrollador": true,
+    libro: {
+        titulo:"Las 48 Leyes del Poder",
+        autor:"Robert Greene y Joost Elffers",
+        url:"https://www.elmayorportaldegerencia.com/Libros/Mercadeo/%5BPD%5D%20Libros%20-%2048%20leyes%20del%20Poder.pdf",
+        fecha: new Date("december 28 2009")   
+    }
+    
+
+}
+
+const fecha_nacimiento = new Date("april 2 2003");
+console.log(fecha_nacimiento)
+
+console.log(sobreMi)
